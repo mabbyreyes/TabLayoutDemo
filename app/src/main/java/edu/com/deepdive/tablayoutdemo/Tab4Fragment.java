fabed9f51c078a239e1090af5615f698605696bd
mabbyreyes <mabbyreyes@gmail.com>
2020-02-23 23:14:08 -0700

    Creating tabbed interface using tablayout

---
+++ app/src/main/java/edu/com/deepdive/tablayoutdemo/Tab4Fragment.java
@@ -0,0 +1,33 @@
+package edu.com.deepdive.tablayoutdemo;
+
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Tab4Fragment extends Fragment {
+
+
+  public Tab4Fragment() {
+    // Required empty public constructor
+  }
+
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_tab4, container, false);
+  }
+
+  public interface OnFragmentInteractionListener {
+    public void onFragmentInteraction(Uri uri);
+  }
+}
